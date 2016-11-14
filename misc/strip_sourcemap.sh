d2894ba047684a2edc87576f21cb96f3c4646b6e
stsloth <stsloth@openmailbox.org>
2016-11-14 14:33:19 +0500

    Add ability to freeze and split sheet views. Add GNU sed support for strip_sourcemap.sh.

--- misc/strip_sourcemap.sh
+++ misc/strip_sourcemap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# strip_sourcemap.sh -- strip sourcemaps from a JS file (missing from uglifyjs) 
+# strip_sourcemap.sh -- strip sourcemaps from a JS file (missing from uglifyjs)
 # Copyright (C) 2014-2015  SheetJS
 
 if [ $# -gt 0 ]; then
 	if [ -e "$1" ]; then
-		sed -i .sheetjs '/sourceMappingURL/d' "$1"
+		sed -i.sheetjs '/sourceMappingURL/d' "$1"
 	fi
 else
 	cat - | sed '/sourceMappingURL/d'
