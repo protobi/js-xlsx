cc73c2dca8628aec7abbb707b03b95c6c5e1e36a
Rob Cross <rwacross@gmail.com>
2017-05-03 12:21:44 -0400

    (feat) compiled fixes freeze pane and styleid

--- xlsx.new.js
+++ xlsx.new.js
@@ -4732,11 +4732,6 @@ function parse_fonts(t, opts) {
         break;
       case '<font':
         break;
-      case '<font/>':
-	      styles.Fonts.push(font);
-		    ;
-	      font = {};
-		    break;
       case '</font>':
         styles.Fonts.push(font);
         ;
@@ -7550,7 +7545,6 @@ var sheetdataregex = /<(?:\w+:)?sheetData>([^\u2603]*)<\/(?:\w+:)?sheetData>/;
 var hlinkregex = /<hyperlink[^>]*\/>/g;
 var dimregex = /"(\w*:\w*)"/;
 var colregex = /<col[^>]*\/>/g;
-var freezeregex = /<pane[^>]*\/>/g;
 /* 18.3 Worksheets */
 function parse_ws_xml(data, opts, rels) {
   if (!data) return data;
@@ -7570,11 +7564,6 @@ function parse_ws_xml(data, opts, rels) {
     var merges = data.match(mergecregex);
     for (ridx = 0; ridx != merges.length; ++ridx)
       mergecells[ridx] = safe_decode_range(merges[ridx].substr(merges[ridx].indexOf("\"") + 1));
-  }
-
-  if (opts.cellStyles && data.indexOf("</sheetView>") !== -1) {
-	  var paneData = data.match(freezeregex);
-	  var freezeData = parse_ws_xml_pane(paneData)
   }
 
   /* 18.3.1.17 cols CT_Cols */
@@ -7609,7 +7598,6 @@ function parse_ws_xml(data, opts, rels) {
   }
   if (mergecells.length > 0) s["!merges"] = mergecells;
   if (columns.length > 0) s["!cols"] = columns;
-  if (freezeData) s["!freeze"] = freezeData;
   return s;
 }
 
@@ -7652,11 +7640,6 @@ function parse_ws_xml_hlinks(s, data, rels) {
       s[addr].l = val;
     }
   }
-}
-
-function parse_ws_xml_pane( paneData) {
-  var fD = parsexmltag(paneData[0], true);
-  return fD;
 }
 
 function parse_ws_xml_cols(columns, cols) {
